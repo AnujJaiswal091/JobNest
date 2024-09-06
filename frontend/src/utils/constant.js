a8c0516d0e9d057fb4f2d5a3aba3a9a8839e1a8a
Anuj <anujjaiswal091@gmail.com>
2024-09-07 02:15:28 +0530

    bug fixes for deployment

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,4 +1,4 @@
-export const USER_API_END_POINT=`${import.meta.env.VITE_SERVER_URL}/user`;
-export const JOB_API_END_POINT=`${import.meta.env.VITE_SERVER_URL}/job`;
-export const APPLICATION_API_END_POINT=`${import.meta.env.VITE_SERVER_URL}/application`;
-export const COMPANY_API_END_POINT=`${import.meta.env.VITE_SERVER_URL}/company`;
+export const USER_API_END_POINT=`${import.meta.env.VITE_SERVER_URL}/api/v1/user`;
+export const JOB_API_END_POINT=`${import.meta.env.VITE_SERVER_URL}/api/v1/job`;
+export const APPLICATION_API_END_POINT=`${import.meta.env.VITE_SERVER_URL}/api/v1/application`;
+export const COMPANY_API_END_POINT=`${import.meta.env.VITE_SERVER_URL}/api/v1/company`;
